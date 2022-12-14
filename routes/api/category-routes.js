4bc8f96bce6670c201312824416ea3719eeb2425
Jennifer Blacutt <jennifer@blacutt.com>
2022-12-14 08:31:19 -0600

    fix get by id, typo

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -18,12 +18,12 @@ router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
-    const categoryData = await Category.findByPk(req.params.id, { include: { model: Product}});
+    const categoryData = await Category.findByPk(req.params.id, { include: { model: Product }});
     if (!categoryData) {
       res.status(404).json({ message: 'Category not found.' });
       return;
     }
-    res.status(200).json(categoryInfo);
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
